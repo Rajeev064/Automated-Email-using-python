d77924e2698610a3ffa759dc431951520447e68b
Rajeev <59364082+Rajeev064@users.noreply.github.com>
2020-02-09 14:45:28 +0530

    Update certificate.py

--- certificate.py
+++ certificate.py
@@ -1,8 +1,10 @@
+#using pil libraries and pandas
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 import pandas as pd
 
+#by using excel 
 x1 = pd.ExcelFile('list2.xlsx')
 df = x1.parse('Sheet1')
 
@@ -10,10 +12,10 @@ df = x1.parse('Sheet1')
 #by using csv
 df = pd.read_csv("list2.csv")
 
-for index, row in df.iterrows():
+for index, row in df.iterrows():  #for loop for making 'n' number of names
   print(index, row['Name'], row['email'])
   n=row['Name']
-  def make_certificate(n):
+  def make_certificate(n):  #function for making n number of certifcates with name enabled on it
     img = Image.open('certificate.jpg')
     draw = ImageDraw.Draw(img)
     selectFont = ImageFont.truetype('LHANDW.ttf', size=64)
@@ -22,4 +24,4 @@ for index, row in df.iterrows():
     draw.text(((width - w) / 2, (height - h) / 2), n, '#7daef7', selectFont)
     img.save('{}.pdf'.format(n))
     img.save('{}.png'.format(n))
-  make_certificate(n)
+  make_certificate(n) #function call
